4fcc56867f123ef27772fbef91ca9a914a2e00a9
Taha <98838967+Taha-Hassan-Git@users.noreply.github.com>
2024-01-27 16:10:46 +0000

    plan page uses agent hook

--- app/page.tsx
+++ app/page.tsx
@@ -9,6 +9,7 @@ import { Run } from "openai/resources/beta/threads/runs/runs.mjs";
 import { createThread, getMessages, postMessage } from "./utils/openAi";
 import { Messages } from "./_components/Messages";
 import { exampleSections, usePortfolioDispatch } from "./_store/store";
+import { useAgent } from "./_hooks/useAgent";
 export interface ChatGPTMessage {
   role: "assistant" | "user";
   content: string;
@@ -21,118 +22,53 @@ export type RunStates =
 
 export default function Home() {
   const dispatch = usePortfolioDispatch();
-  const [inputContent, setInputContent] = useState<string>("");
-  const [error, setError] = useState<string | null>(null);
-  const [runState, setRunState] = useState<RunStates>({ name: "ready" });
-  const [messages, setMessages] = useLocalStorage<ChatGPTMessage[]>(
-    "messages",
-    []
-  );
-  const [thread, setThread] = useLocalStorage<Thread | null>("thread", null);
-
-  const assistantsArr = [
-    { name: "skeleton", id: "asst_3aXwBiUZFqzcoSLUdGeFLGiG" },
-  ];
-
-  const onSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-
-    const updatedMessages: ChatGPTMessage[] = [
-      ...messages,
-      { role: "user", content: inputContent },
-    ];
-
-    setMessages(updatedMessages);
-    setInputContent("");
-
-    try {
-      let skeletonThread = thread;
-
-      const skeletonAssistant = assistantsArr.find(
-        (assistant) => assistant.name === "skeleton"
-      );
-
-      if (skeletonThread === null) {
-        skeletonThread = await createThread();
-        setThread(skeletonThread);
-      }
-      if (skeletonThread) {
-        await postMessage({
-          updatedMessages,
-          currentAssistantId: skeletonAssistant?.id as string,
-          currentThreadId: skeletonThread.id,
-          setError,
-          setRunState,
-        });
-      }
-    } catch (error: any) {
-      setError(error.message);
-      setTimeout(() => setError(""), 5000);
-    }
-  };
+  const {
+    thread,
+    messages,
+    runState,
+    onSubmit,
+    inputContent,
+    setInputContent,
+    resetThread,
+    error,
+  } = useAgent("skeleton", "plan:");
 
   useEffect(() => {
-    let interval: NodeJS.Timeout;
-    const checkRun = async () => {
-      if (runState.name === "running" && thread) {
-        const response = await fetch("/api/check-run", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            thread: thread,
-            run: runState.run,
-          }),
-        });
-
-        if (response.status !== 200) {
-          const data = await response.json();
-          setError("Error getting response: " + data.statusText);
-          setTimeout(() => setError(""), 1000);
-        } else {
-          const run: Run = await response.json();
-          if (run.status === "completed") {
-            setRunState({ name: "ready" });
-            const updatedMessages = await getMessages(run.thread_id);
-            console.log(updatedMessages);
-            const content = updatedMessages[0].content[0];
-
-            if ("text" in content) {
-              const aiResponse = content.text.value;
-              setMessages([
-                ...messages,
-                { role: "assistant", content: aiResponse },
-              ]);
-              checkProject(aiResponse, dispatch);
-            } else {
-              console.log(
-                "Error getting text value from response, it may have returned an image"
-              );
-            }
-          }
-          if (run.status === "requires_action") {
-            console.log("call function");
-            setRunState({ name: "error" });
-          }
+    const checkProject = (aiResponse: string, dispatch: any) => {
+      //check if the message has triple quotes """message"""
+      const tripleQuoteRegex = /"""(.*?)"""/;
+      const tripleQuoteMatches = aiResponse.match(tripleQuoteRegex);
+      if (tripleQuoteMatches) {
+        console.log("contains triple brackets");
+        //get the text inside the triple brackets
+        const tripleQuoteText = tripleQuoteMatches[0].replace(
+          tripleQuoteRegex,
+          "$1"
+        );
+        //check if the text is a valid object
+        const tripleQuoteObject = JSON.parse(tripleQuoteText);
+        // if it is, dispatch the action
+        if (tripleQuoteObject) {
+          dispatch({
+            type: "SET_PORTFOLIO",
+            payload: { id: 0, sections: exampleSections },
+          });
         }
-      } else {
-        clearInterval(interval);
       }
     };
-
-    interval = setInterval(checkRun, 500);
-
-    return () => {
-      clearInterval(interval);
-    };
-  }, [dispatch, messages, runState, setMessages, thread]);
+    if (messages.length > 0) {
+      const lastMessage = messages[messages.length - 1];
+      if (lastMessage.role === "assistant") {
+        checkProject(lastMessage.content, dispatch);
+      }
+    }
+  }, [dispatch, messages]);
 
   return (
     <div className="flex flex-col h-screen w-full items-center justify-between bg-gray-50">
       <div className="flex flex-col items-start p-3 gap-3 overflow-scroll w-full ">
         <button
-          onClick={() => resetThread(setThread, setMessages)}
+          onClick={() => resetThread()}
           className="text-sm rounded py-1 px-2 border border-gray-300 bg-gray-100"
         >
           {thread ? "Reset" : "No thread"}
@@ -170,38 +106,3 @@ export default function Home() {
     </div>
   );
 }
-
-const resetThread = (
-  setThread: LocalStorageSetter<Thread | null>,
-  setMessages: React.Dispatch<SetStateAction<ChatGPTMessage[]>>
-) => {
-  setThread(null);
-  setMessages([]);
-};
-
-const checkProject = (aiResponse: string, dispatch: any) => {
-  dispatch({
-    type: "SET_PORTFOLIO",
-    payload: { id: 0, sections: exampleSections },
-  });
-  //check if the message has triple quotes """message"""
-  const tripleQuoteRegex = /"""(.*?)"""/;
-  const tripleQuoteMatches = aiResponse.match(tripleQuoteRegex);
-  if (tripleQuoteMatches) {
-    console.log("contains triple brackets");
-    //get the text inside the triple brackets
-    const tripleQuoteText = tripleQuoteMatches[0].replace(
-      tripleQuoteRegex,
-      "$1"
-    );
-    //check if the text is a valid object
-    const tripleQuoteObject = JSON.parse(tripleQuoteText);
-    // if it is, dispatch the action
-    if (tripleQuoteObject) {
-      dispatch({
-        type: "SET_PORTFOLIO",
-        payload: { id: 0, sections: exampleSections },
-      });
-    }
-  }
-};
